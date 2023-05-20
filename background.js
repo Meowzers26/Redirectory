c9a371b03242ba888070694e6a4be5172eb69a74
Sophia <sophia.izwan@gmail.com>
2023-05-19 23:33:24 -0600

    redirects all youtube searches to [insert here]

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.webRequest.onBeforeRequest.addEventListener("change", function(details) {
-    return {redirectUrl: "https://www.youtube.com/watch?v=tyu92s0cuCk"};
-},  {
-    urls: ["https://youtube.com/*"],
-    types: ["main_frame", "sub_frame"],
-},  [
-    "blocking"
-]);
